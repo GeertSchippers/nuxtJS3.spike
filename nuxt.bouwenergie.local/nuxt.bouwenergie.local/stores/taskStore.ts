2af930c9d1462861ddceddb2f80c8fcacdcc6d92
geert <geert@bouwenergie.nl>
2023-05-17 12:55:12 +0000

    remove not used api calls

--- nuxt.bouwenergie.local/nuxt.bouwenergie.local/stores/taskStore.ts
+++ nuxt.bouwenergie.local/nuxt.bouwenergie.local/stores/taskStore.ts
@@ -24,49 +24,21 @@ export const useTaskStore = defineStore('taskStore', {
         async getTasks() {
             this.loading = true;
             const { data }  = await useFetch(() => '/api/tasks')
-            
+
             this.tasks = data.value.tasks
             this.loading = false
         },
         async addTask(task) {
             this.tasks.push(task)
-
-            const res = await fetch('/api/tasks', {
-                method: "POST",
-                body: JSON.stringify(task),
-                headers: {'content-Type' : 'application/json'}
-            })
-
-            if(res.error) {
-                console.log(res.error)
-            }
         },
         async deleteTask(id) {
             this.tasks = this.tasks.filter(task => {
                 return task.id !== id
             })
-
-            const res = await fetch('/api/tasks/' + id, {
-                method: "DELETE",
-            })
-
-            if(res.error) {
-                console.log(res.error)
-            }
         },
         async toggleFav(id) {
             const task = this.tasks.find(task => task.id === id)
             task.isFav = !task.isFav
-
-            const res = await fetch('/api/tasks/' + id, {
-                method: "POST",
-                body: JSON.stringify({  isFav: task.isFav}),
-                headers: {'content-Type' : 'application/json'}
-            })
-
-            if(res.error) {
-                console.log(res.error)
-            }
         }
     }
 })
